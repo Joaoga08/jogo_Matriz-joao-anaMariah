e259144dc011eaaecfede1e574c61d3e75bd488d
Joaoga08 <wandreyschelbauerjoaogabriel@gmail.com>
2024-09-18 11:03:56 -0300

    implementação do metodo

--- My project (10)/Assets/GameManager.cs
+++ My project (10)/Assets/GameManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -10,6 +12,29 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+        grade = grade[linhas, colunas];
+
+
+    }
+    public void ChecarGrade()
+
+    {
+
+        int posicao = 0;
+        for (int i = 0; i < grade.Length; i++)
+        {
+            for(int j = 0; j <grade.Length; j++ )
+            {
+                posicao = colunas = linhas;
+            }
+        }
+
+        Instantiate(blocoPrefab, new Vector3(posicao, posicao), Quaternion.identity);
+
+        grade[1,1] = posicao;
+      blocoPrefab  = Instantiate(AreaPrefab, new Vector3(posicao, posicao), Quaternion.identity);
+
+
     }
     #endregion
     public GameObject jogador1;
@@ -19,9 +44,15 @@ public class GameManager : MonoBehaviour
     int colunas;
     float espacamento;
 
-    Bloco[,]blocos;
+   public GameObject blocoPrefab;
+    GameObject bloco;
 
+    Bloco[,] grade;
+
+   
     int contador;
+
+    
 
 
     // Start is called before the first frame update
