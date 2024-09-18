ea3997fa313d35092d29d3bf1c65840173417464
Joaoga08 <wandreyschelbauerjoaogabriel@gmail.com>
2024-09-18 13:52:51 -0300

    implentação do metodo

--- My project (10)/Assets/Bloco.cs
+++ My project (10)/Assets/Bloco.cs
@@ -7,5 +7,40 @@ public class BlocoTerritorio : MonoBehaviour
     private bool conquistado;
     private SpriteRenderer spriteRenderer;
     private int jogadorDono;
-   
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        InicializarBloco(Color.white);
+    }
+
+    private void InicializarBloco(Color cor)
+    {
+        spriteRenderer.color = cor;
+        conquistado = false;
+        jogadorDono = 0;
+    }
+
+    private void AtualizarCor(Color novaCor)
+    {
+        spriteRenderer.color = novaCor;
+    }
+
+
+    public void AlterarConquista(bool jogador1, Color corDoJogador)
+    {
+        conquistado = true;
+        AtualizarCor(corDoJogador);
+
+        jogadorDono = jogador1 ? 1 : 2;
+        GameManager.Instance.NotificarTerritorioConquistado(this);
+    }
+    public bool PegarConquistado()
+    {
+        return conquistado;
+    }
+
+    public int PegarJogadorDono()
+    {
+        return jogadorDono;
+    }
 }
