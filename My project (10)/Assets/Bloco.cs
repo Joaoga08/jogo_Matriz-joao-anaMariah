8aa53f85193b1c3bf8b7840cb9b0e0e23307d904
Joaoga08 <wandreyschelbauerjoaogabriel@gmail.com>
2024-09-18 13:52:26 -0300

    criação de variaveis

---
+++ My project (10)/Assets/Bloco.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocoTerritorio : MonoBehaviour
+{
+    private bool conquistado;
+    private SpriteRenderer spriteRenderer;
+    private int jogadorDono;
+   
+}
