f6107443e0a4fd82b1126c7e95ba66f29c400aec
AnaChaves07 <anajuliaschaves7@gmail.com>
2024-09-18 14:23:27 -0300

    terminado

--- My project (10)/Assets/GameManager.cs
+++ My project (10)/Assets/GameManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,58 +10,90 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        grade = grade[linhas, colunas];
-
-
-    }
-    public void ChecarGrade()
-
-    {
-
-        int posicao = 0;
-        for (int i = 0; i < grade.Length; i++)
-        {
-            for(int j = 0; j <grade.Length; j++ )
-            {
-                posicao = colunas = linhas;
-            }
-        }
-
-        Instantiate(blocoPrefab, new Vector3(posicao, posicao), Quaternion.identity);
-
-        grade[1,1] = posicao;
-      blocoPrefab  = Instantiate(AreaPrefab, new Vector3(posicao, posicao), Quaternion.identity);
-
-
     }
     #endregion
-    public GameObject jogador1;
-    public GameObject jogador2;
     [SerializeField] GameObject AreaPrefab;
-    int linhas;
-    int colunas;
-    float espacamento;
-
-   public GameObject blocoPrefab;
-    GameObject bloco;
-
-    Bloco[,] grade;
-
    
+
+    Bloco[,]blocos;
+
     int contador;
 
-    
-
-
-    // Start is called before the first frame update
+    public int linhas = 5;
+    public int colunas = 5;
+    public float espacamento = 1.0f;
+    private Bloco[,] grade;
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private int contadorConquistados = 0;
     void Start()
     {
-        
+        CriarGrade();
+        PosicionarJogadores();
     }
-
-    // Update is called once per frame
-    void Update()
+    void CriarGrade()
     {
-        
+        grade = new Bloco[linhas, colunas];
+        for (int linha = 0; linha < linhas; linha++)
+        {
+            for (int coluna = 0; coluna < colunas; coluna++)
+            {
+                Vector3 posicao = new Vector3(coluna * espacamento, 0, linha * espacamento);
+                GameObject blocoObj = Instantiate(Resources.Load("Bloco"), posicao, Quaternion.identity) as GameObject;
+                grade[linha, coluna] = blocoObj.GetComponent<Bloco>();
+            }
+        }
     }
+    void PosicionarJogadores()
+    {
+        Vector3 posicaoJogador1 = new Vector3(0, 0, 0);
+        Vector3 posicaoJogador2 = new Vector3((colunas - 1) * espacamento, 0, (linhas - 1) * espacamento);
+        jogador1 = Instantiate(Resources.Load("Jogador"), posicaoJogador1, Quaternion.identity) as Jogador;
+        jogador2 = Instantiate(Resources.Load("Jogador"), posicaoJogador2, Quaternion.identity) as Jogador;
+        Camera.main.transform.position = new Vector3(colunas * espacamento / 2, 10, linhas * espacamento / 2);
+        Camera.main.orthographicSize = Mathf.Max(linhas, colunas) * espacamento / 2;
+    }
+    public void ConquistarTerritorio(Bloco blocoConquistado, Jogador jogador)
+    {
+        if (!blocoConquistado.Conquistado)
+        {
+            blocoConquistado.Conquistado = true;
+            contadorConquistados++;
+            if (contadorConquistados == linhas * colunas)
+            {
+                FimDeJogo();
+            }
+        }
+    }
+    private void FimDeJogo()
+    {
+        int contadorJogador1 = 0;
+        int contadorJogador2 = 0;
+        foreach (Bloco bloco in grade)
+        {
+            if (bloco.Jogador == jogador1)
+            {
+                contadorJogador1++;
+            }
+            else if (bloco.Jogador == jogador2)
+            {
+                contadorJogador2++;
+            }
+        }
+        string vencedor;
+        if (contadorJogador1 > contadorJogador2)
+        {
+            vencedor = "Jogador 1 venceu!";
+        }
+        else if (contadorJogador2 > contadorJogador1)
+        {
+            vencedor = "Jogador 2 venceu!";
+        }
+        else
+        {
+            vencedor = "Empate!";
+        }
+        Debug.Log(vencedor);
+    }
+}
 }
